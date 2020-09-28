ef91ec9cc6645d8953b44ad2681568153250afa0
Maurice Putz <mauriceputz2000@gmail.com>
2020-09-28 16:31:13 +0200

    finshing point 2of exercise

--- 01_processes/src/aba.cpp
+++ 01_processes/src/aba.cpp
@@ -1,7 +1,32 @@
 #include <iostream>
+#include <thread>
+#include <unistd.h>
+#include <chrono>
+#include <csignal>
+#include <sys/wait.h>
 
 using namespace std;
 
+//declaration
+pid_t pid1;
+pid_t pid2;
+int state;
+
 int main() {
-    cout << "test" << endl;
+    pid1 = fork();
+
+    
+    if (pid1 == 0) {    //child process
+        while(true) {
+            cout << "A" << endl;
+            sleep(1);
+        }
+    } else if (pid1 > 0) { //parent process
+        while(true) {
+            cout << "B" << endl;
+            sleep(1);
+        }
+    } else {
+        cout << "fork failed" << endl;
+    }
 }
