12909993867f237a2c487bc21577548712b678ee
Maurice Putz <mauriceputz2000@gmail.com>
2020-09-29 13:11:44 +0200

    +finishing task 2 in unit 02

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -4,6 +4,20 @@
 
 using namespace std;
 
+class Car {
+    public:
+    void operator()(string car_type) {
+        int round = 0;
+        cout << "START!" << endl;
+        while(true) {
+            this_thread::sleep_for(chrono::seconds(1));
+            round++;
+            cout << "Car: " << car_type << "; Round: " << round << endl;
+            cout << flush;
+        }
+    };
+};
+
 void letCarRace(string car_type) {
     int round = 0;
     cout << "START!" << endl;
@@ -16,6 +30,9 @@ void letCarRace(string car_type) {
 }
 
 int main() {
-    thread car1 (letCarRace, "lada taiga"); 
-    car1.join();
+    //thread car1 (letCarRace, "lada taiga"); 
+    //car1.join();
+    Car car_object;
+    thread car2 (car_object, "Opel Corsa");
+    car2.join();
 }
