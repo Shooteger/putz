bdae8048dcb7339e6f04f5d3386d4b13d95e59eb
Maurice Putz <mauriceputz2000@gmail.com>
2020-09-29 13:17:55 +0200

    +fixing bug with thread join in unit02 and adding task 5

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -8,7 +8,6 @@ class Car {
     public:
     void operator()(string car_type) {
         int round = 0;
-        cout << "START!" << endl;
         while(true) {
             this_thread::sleep_for(chrono::seconds(1));
             round++;
@@ -19,19 +18,18 @@ class Car {
 
 void letCarRace(string car_type) {
     int round = 0;
-    cout << "START!" << endl;
     while(true) {
         this_thread::sleep_for(chrono::seconds(1));
         round++;
-        cout << "Car: " << car_type << "; Round: " << round << endl;
-        cout << flush;
+        cout << "Car: " << car_type << "; Round: " << round << "\n" << flush;
     }
 }
 
 int main() {
-    //thread car1 (letCarRace, "lada taiga"); 
-    //car1.join();
     Car car_object;
+
+    thread car1 (letCarRace, "lada taiga");     
     thread car2 (car_object, "Opel Corsa");
+    car1.join();
     car2.join();
 }
