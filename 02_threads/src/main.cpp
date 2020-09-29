ae93dc440a22237bcae7644a61dcd2d5f1bc886b
Maurice Putz <mauriceputz2000@gmail.com>
2020-09-29 12:23:29 +0200

    +finishing first task of 02 unit

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -1,6 +1,21 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
 
 using namespace std;
 
+void letCarRace(string car_type) {
+    int round = 0;
+    cout << "START!" << endl;
+    while(true) {
+        this_thread::sleep_for(chrono::seconds(1));
+        round++;
+        cout << "Car: " << car_type << "; Round: " << round << endl;
+        cout << flush;
+    }
+}
+
 int main() {
-    return 0;
+    thread car1 (letCarRace, "lada taiga"); 
+    car1.join();
 }
