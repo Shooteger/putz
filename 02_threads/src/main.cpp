14225b3c3a0cf2f7cc9e5b7eabf35eeb9a323537
Maurice Putz <mauriceputz2000@gmail.com>
2020-10-05 15:40:18 +0200

    +adding task 9 for unit 02

--- 02_threads/src/main.cpp
+++ 02_threads/src/main.cpp
@@ -10,12 +10,12 @@ class Car {
     public:
     double total_time;
 
-    void operator()(string car_type) {
+    void operator()(string car_type, int laps) {
         int roundCar = 0;
         random_device r1;
         mt19937 gen{r1()};
         double lap_time;
-        while(roundCar < 10) {
+        while(roundCar < laps) {
             uniform_real_distribution<> dis{1000, 10000};
             int sleep_time = dis(gen);
             this_thread::sleep_for(chrono::milliseconds(sleep_time));
@@ -51,9 +51,42 @@ double letCarRace(string car_type) {
     return total_lap_time;
 }
 
-int main() {
-    Car car_object;
+void help() {
+    cout << "Usage: contest [-h | --help | LAPS]\n";
+    exit(0);
+}
 
+void error(string message="") {
+    if (message != "") {
+        cerr << message << "\n";
+    }
+    cerr << "Run with --help for more information.\n";
+    exit(1);    
+}
+
+int main(int args, char* argv[]) {
+    if (args != 2) {
+        error("Minimum of one parameter required.");
+    } else {
+        string tmp_laps = argv[1]; //at this point argv[1], tmp_laps could be -h or --help
+        if (tmp_laps == "-h" || tmp_laps == "--help") {
+            help();
+        } else {
+            try {
+                if (stoi(argv[1]) >= 1 && stoi(argv[1]) < 16) {
+                    error("Out of range (1 <= LAP'S < 16): " + tmp_laps);
+                } else {
+                    Car car_object;
+                    thread car_thread (ref(car_object), "Opel Corsa", tmp_laps);
+                }
+            } catch(const invalid_argument& ia) {
+                error("Could not convert: " + tmp_laps + "\n" + ia.what());
+            }
+            
+        }
+    }
+
+    /*
     future<double> car1 = async(letCarRace, "Porsche Cayman S");
 
     //thread car1 (letCarRace, "Porsche Cayman S");     
@@ -65,8 +98,11 @@ int main() {
     double car2Time = car_object.get_total_time();
 
     if (car1Time > car2Time) {
-        cout << "Winner: Porsche Cayman S\n" << "Total Time: " << car2Time << endl;
+        cout << "\nWinner: Porsche Cayman S\n" << "Total Time: " << car2Time << "\n";
+        cout << "Loser: Opel Corsa S\n" << "Total Time: " << car1Time << "\n";
     } else {
-        cout << "Winner: Opel Corsa\n" << "Total Time: " << car1Time  << "s" << endl;;
+        cout << "\nWinner: Opel Corsa\n" << "Total Time: " << car1Time  << "s" << "\n";
+        cout << "Loser: Porsche Cayman S\n" << "Total Time: " << car2Time  << "s" << "\n";
     }
+    */
 }
