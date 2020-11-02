9115827a899c710c844b33608996e6be7665c13a
Maurice Putz <mauriceputz2000@gmail.com>
2020-11-02 14:12:18 +0100

    +finishing task 3 heading to task 4 unti 4

--- 04_sync2/src/main.cpp
+++ 04_sync2/src/main.cpp
@@ -1,6 +1,23 @@
+#include <iostream>
+#include <thread>
+#include <functional>
+
+#include "work_queue.h"
+#include "work_packet.h"
 
 using namespace std;
 
 int main() {
-    return 0;
+    WorkQueue wq{10};
+
+    int cnt{0};
+
+    while (true) {
+        //time_out = dis(gen);
+        this_thread::sleep_for(chrono::milliseconds((int) 500));
+        wq.push(WorkPacket{cnt});
+        cout << "B: Submitted work packet " << cnt << "\n";
+        cnt++;
+    }
+
 }
