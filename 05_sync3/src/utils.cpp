0f3e9ae7327b4c2106cc7247944d00878307a97d
Maurice Putz <mauriceputz2000@gmail.com>
2020-11-18 11:50:51 +0100

    +finishing task 4 to 5 unti5

--- 05_sync3/src/utils.cpp
+++ 05_sync3/src/utils.cpp
@@ -7,3 +7,15 @@
 using namespace std;
 
 recursive_mutex out_mtx;
+
+void println() {
+    lock_guard<recursive_mutex> lg{out_mtx};
+    cout << "\n";
+}
+
+template<typename T, typename... Rest>
+void println(const T& word, const Rest&... rest) {
+    lock_guard<recursive_mutex> lg{out_mtx};
+    cout << word << ' ';
+    println(rest);
+}
