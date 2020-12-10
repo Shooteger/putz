eae55b02e88f2f4cf2c7a38f036dcb2052e27c8e
Maurice Putz <mauriceputz2000@gmail.com>
2020-12-10 14:30:05 +0100

    +finishing task1 in untit6

--- 06_future/src/main.cpp
+++ 06_future/src/main.cpp
@@ -1,6 +1,20 @@
+#include <iostream>
+#include <vector>
 
+#include "calc_factors.h"
+#include "CLI11.hpp"
+
+
+using namespace CLI;
 using namespace std;
 
-int main() {
-    return 0;
+int main(int argc, char* argv[]) {
+    vector<InfInt> numbers;
+
+    vector<int> ref_nums{1, 2, 3, 4, 5, 6, 7, 8, 9, 0};
+
+    App app{"Factor numbers"};
+    app.add_option("number", numbers, "numbers to factor")->check(CLI::Number);
+
+    CLI11_PARSE(app, argc, argv);
 }
