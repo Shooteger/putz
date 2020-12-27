cc7768674cd448babea230b443dfd7a944325ce8
Shooteger <mauriceputz2000@gmail.com>
2020-12-27 16:16:57 +0100

    +adding task 3-4 unit 7

--- 07_berkeley/src/main.cpp
+++ 07_berkeley/src/main.cpp
@@ -2,14 +2,28 @@
 #include <chrono>
 #include <thread>
 
+#include "CLI11.hpp"
 #include "clock.h"
 
 using namespace std;
+using namespace CLI;
+
+class TimeSlave {
+    public:
+        TimeSlave(string name_, int hours=0, int minutes=0, int seconds=0) : name{name_}, curr_time{name_, hours, minutes, seconds} {}
+    private:
+        string name;
+        Clock curr_time;
+};
 
 int main() {
     
-    thread clock{Clock("testclock")};
-    clock.join();
+    //thread clock{Clock("testclock")};
+    //clock.join();
 
-        
+    thread t1{TimeSlave{"slave1", 10, 16, 0}};
+    thread t2{TimeSlave{"slave2", 10, 17, 0}};
+
+    t1.join();
+    t2.join();
 }
